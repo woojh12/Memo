2f871906a3e23c659584c7534fec95a270d62b99
megait <megait@gmail.com>
2024-09-05 20:43:53 +0900

    [우재현]  - 로그인 회원가입 마크업  - thymeleaf layout 라이브러리 적용
    #2

---
+++ src/main/java/com/jhmemo/memo/user/UserController.java
@@ -0,0 +1,22 @@
+package com.jhmemo.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/join-view")
+	public String inputJoin()
+	{
+		return "user/join";
+	}
+	
+	@GetMapping("/login-view")
+	public String inputLogin()
+	{
+		return "user/login";
+	}
+}
